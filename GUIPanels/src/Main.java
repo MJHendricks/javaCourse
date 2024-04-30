e3fc1ecb1da7cbe2cf6162975bc0151ddd16c126
MJHendricks <matthew.joshua.hendricks@gmail.com>
2024-04-30 14:40:14 +0200

    modified:   GUIPanels/src/Main.java

--- GUIPanels/src/Main.java
+++ GUIPanels/src/Main.java
@@ -1,5 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        JLabel
+        JLabel label = new JLabel();
+        label.setText("Hi");
+
+//        JPanel
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.red);
+        redPanel.setBounds(0,0,100,100);
+
+        JPanel bluePanel = new JPanel();
+        bluePanel.setBackground(Color.blue);
+        bluePanel.setBounds(100,0,100,100);
+
+        JPanel greenPanel = new JPanel();
+        greenPanel.setBackground(Color.green);
+        greenPanel.setBounds(0,100,200,100);
+
+//        frame
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setSize(720,420);
+        frame.setVisible(true);
+
+        greenPanel.add(label);
+
+        frame.add(redPanel);
+        frame.add(bluePanel);
+        frame.add(greenPanel);
+
+
     }
 }
