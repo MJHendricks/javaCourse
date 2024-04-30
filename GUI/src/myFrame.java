5eb6de28597dacf44d5dec574844c86dc1013ae3
MJHendricks <matthew.joshua.hendricks@gmail.com>
2024-04-30 10:53:48 +0200

    30042024

--- GUI/src/myFrame.java
+++ GUI/src/myFrame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 public class myFrame extends JFrame {
@@ -11,6 +12,7 @@ public class myFrame extends JFrame {
 
         //add image to frame
         ImageIcon image = new ImageIcon("logo.png");
+        Border border = BorderFactory.createLineBorder(Color.green,3);
 
         label.setIcon(image);
 
@@ -20,11 +22,25 @@ public class myFrame extends JFrame {
         // text styling
         label.setForeground(new Color(0x00AAFF)); //text color
         label.setFont(new Font("MV Boli",Font.BOLD, 20));
+        label.setIconTextGap(10);
+
+        //bg color
+        label.setBackground(Color.black);
+        label.setOpaque(true);
+        label.setBorder(border);
+
+        //alignment
+        label.setVerticalAlignment(JLabel.CENTER); //TOP, CENTER or BOTTOM for icon + text
+        label.setHorizontalAlignment(JLabel.CENTER);
+
+        label.setBounds(0,0,300,300); //x pos, y pos, width, height
 
         //setup frame
         this.setTitle("GUI");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //ALLOWS X TO CLOSE APPLICATION
         this.setSize(420,360); // set x and y
+        this.setLayout(null);
+
         this.setVisible(true); // make frame visible
 
         // change icon
